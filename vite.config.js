ca45057540526104ee922ef1da76f83d2ca35515
Chia1104 <yuyuchia7423@gmail.com>
2023-02-20 20:36:01 +0800

    :adhesive_bandage: Fix.

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,10 @@
 import { sveltekit } from "@sveltejs/kit/vite";
-import adapter from "@sveltejs/adapter-node";
 
 /** @type {import('vite').UserConfig} */
 const config = {
   plugins: [sveltekit()],
   test: {
     include: ["src/**/*.{test,spec}.{js,ts}"],
-  },
-  kit: {
-    adapter: adapter({ out: "build" }),
   },
 };
 
